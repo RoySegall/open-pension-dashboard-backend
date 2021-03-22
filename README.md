494730fdd37b46181e0b51b94583b7aeffc8fba1
RoySegall <roy@segall.io>
2021-03-22 19:33:53 +0200

    Adding the basic graphql

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # open_pension_dashboard_backend
+Todo:
+* [X] Typescript
+* [X] GraphQL - apollo
+* [ ] Kafka
+* [ ] Queue - typescript
+* [ ] Mongo
+* [ ] Sockets - pusher
+* [ ] Cli - add users, view stuff
+* [ ] Email - sendgrid
 
-Backend for open pension dashboard
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-export * from './lib/async';
-export * from './lib/number';
+import { ApolloServer } from 'apollo-server';
+
+import { resolvers } from './server/resolvers';
+import { typeDefs } from './server/schema';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
